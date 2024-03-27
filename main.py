d29c7af62187169812d810eaed372e49c88c7013
blackhatdog <76096163+blackhatdog@users.noreply.github.com>
2024-03-27 13:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import serial
 import subprocess as sub
 import webbrowser as wb
-ser = serial.Serial('COM6',115200, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE) #Tried with and without the last 3 parameters, and also at 1Mbps, same happens.
+ser = serial.Serial('COM6',115200, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE) 
 ser.flushInput()
 ser.flushOutput()
 while True:
